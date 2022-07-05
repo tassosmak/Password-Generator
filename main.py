17aeb9474012b6c1782eb164c1f22ca4f1192ec8
Tassos Makrostergios <86314553+TASMAK@users.noreply.github.com>
2022-07-05 14:11:56 +0300

    Save Added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import string
 from random import random, shuffle, choice
 
+
 #characters = list(string.ascii_lowercase, string.ascii_uppercase, string.digits, + "!@#$%^&*()")
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 def gen():
@@ -15,6 +16,13 @@ def gen():
     for i in range(length):
         password.append(choice(characters))
 
+    password_str = ''.join(str(e) for e in password)
+
     print("".join(password))
+    save_ask = input("Would You like to export the password to a txt?\n if YES press y")
+    if save_ask == "y" or save_ask == "Y":
+        f = open("export.log", "w")
+        f.write(password_str)
+        print('DONE')
 
 gen()
